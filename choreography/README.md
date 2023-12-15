db69d05989fdb9ff64d27a1197fd4873e8f0a9b1
smaug <cima19056@gmail.com>
2023-12-15 15:11:16 +0300

    add diagrams & swagger implementation

---
+++ choreography/README.md
@@ -0,0 +1,26 @@
+### Command Descriptions:
+
+1. **Install Graphviz Package:**
+
+```bash
+sudo apt install graphviz -y
+```
+
+2. **Create and Activate a Python Virtual Environment:**
+
+```bash
+python -m venv myenv
+source myenv/bin/activate
+```
+
+3. **Install Graphviz Python Library:**
+
+```bash
+pip install graphviz
+```
+
+4. **Generate Diagram Using Python Script:**
+
+```bash
+python3 generate_diagram.py
+```
---
+++ choreography/structure_diagram.py
@@ -0,0 +1,55 @@
+from graphviz import Digraph
+
+# Initialize Graphviz object
+dot = Digraph()
+
+# Define entities
+dot.node('User', 'User')
+dot.node('Frontend', 'Frontend')
+dot.node('Content', 'Content')
+dot.node('Backend', 'Backend')
+dot.node('DB', 'Database (SQLite)')
+dot.node('Admin', 'Admin')
+
+# Define abstraction blocks
+dot.node('Pinakothek', 'Pinakothek\nWeb Application')
+dot.node('Authorization', 'Authorization\n& Authentication')
+dot.node('ContentManagement', 'Content Management')
+dot.node('ContentDisplay', 'Content Display')
+dot.node('MediaProcessing', 'Media Processing')
+dot.node('DataStorage', 'Data Storage')
+
+# Define fields and methods within the abstraction blocks
+dot.node('Fields1', 'Fields: \n- User Data\n- Login Sessions', shape='box')
+dot.node('Methods1', 'Methods: \n- Register User\n- Log In\n- Log Out', shape='box')
+dot.node('Fields2', 'Fields: \n- Upload/Delete/Edit\n- Media Description', shape='box')
+dot.node('Methods2', 'Methods: \n- Upload Media\n- Delete Media\n- Edit Media', shape='box')
+dot.node('Fields3', 'Fields: \n- Gallery View\n- Pagination\n- Filtering', shape='box')
+dot.node('Fields4', 'Fields: \n- Pillow (Images)\n- FFmpeg (Videos)', shape='box')
+dot.node('Fields5', 'Fields: \n- SQLite\n- Stored Files Info', shape='box')
+
+# Add dependency connections between abstraction blocks and entities
+dot.edge('Pinakothek', 'Authorization')
+dot.edge('Pinakothek', 'ContentManagement')
+dot.edge('Pinakothek', 'ContentDisplay')
+dot.edge('Pinakothek', 'MediaProcessing')
+dot.edge('Pinakothek', 'DataStorage')
+
+dot.edge('Authorization', 'Fields1')
+dot.edge('Authorization', 'Methods1')
+dot.edge('ContentManagement', 'Fields2')
+dot.edge('ContentManagement', 'Methods2')
+dot.edge('ContentDisplay', 'Fields3')
+dot.edge('MediaProcessing', 'Fields4')
+dot.edge('DataStorage', 'Fields5')
+
+# Add connections between entities and abstraction blocks
+dot.edge('User', 'Authorization')
+dot.edge('Frontend', 'ContentDisplay')
+dot.edge('Content', 'ContentManagement')
+dot.edge('Backend', 'MediaProcessing')
+dot.edge('DB', 'DataStorage')
+dot.edge('Admin', 'Authorization')
+
+# Save and render the diagram
+dot.render('pinakothek_structure_updated', format='png', view=True)
